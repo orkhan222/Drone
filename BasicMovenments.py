34426be38b07a0bcb6dd46202a8cb2fb565e1e5e
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-02-07 11:07:43 +0400

    BasicMovenments

--- BasicMovenments.py
+++ BasicMovenments.py
@@ -1,6 +1,12 @@
-from djitellopy import  tello
-# from time import sleep
+from djitellopy import tello
+from time import sleep
 
 me = tello.Tello()
 me.connect()
 print(me.get_battery())
+
+me.takeoff()
+me.send_rc_control(0 , 50 , 0 , 0)
+slepp(2)
+me.send_rc_control(0 , 0 , 0 , 0)
+me.land()
