f107b4461d4f8167b73874d51a979d73d8f5b58f
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-02-08 13:20:59 +0400

    VideoCapture

--- BasicMovenments.py
+++ BasicMovenments.py
@@ -1,12 +1,35 @@
-from djitellopy import tello
-from time import sleep
+import numpy as np
+import cv2
 
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
+videoCapture = cv2.VideoCapture(0)
+prevCircle = None
+dist = lambda x1,y1,x2,y2: (x1-x2)**2*(y1-y2)**2
 
-me.takeoff()
-me.send_rc_control(0 , 50 , 0 , 0)
-slepp(2)
-me.send_rc_control(0 , 0 , 0 , 0)
-me.land()
+while True:
+    ret, frame = videoCapture.read()
+    if not ret:
+        break
+
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurFrame = cv2.GaussianBlur(grayFrame, (17,17), 0)
+
+    circles = cv2.HoughCircles(blurFrame, cv2.HOUGH_GRADIENT, 1, 100,
+                                param1=80,param2=30,minRadius=55,maxRadius=200)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        chosen = None
+        for i in circles[0, :]:
+            if chosen is None: chosen = i
+            if prevCircle is not None:
+                if dist(chosen[0],chosen[1],prevCircle[0],prevCircle[1]) <= dist(i[0],i[1],prevCircle[0],prevCircle[1]):
+                    chosen = i
+        cv2.circle(frame, (chosen[0], chosen[1]), 1, (0,100,100), 3)
+        cv2.circle(frame, (chosen[0], chosen[1]), chosen[2], (255,0,255), 3)
+        prevCircle = chosen
+
+    cv2.imshow("circles", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+videoCapture.release()
+cv2.destroyAllWindows()
